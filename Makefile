e1d556dca355ffeccbf410e191f066464b0b8091
Irene Mei <irene.mei@studio.unibo.it>
2021-07-05 11:29:54 +0200

    new update

--- Makefile
+++ Makefile
@@ -7,10 +7,10 @@ ensembl_link = https://stockholmuniversity.box.com/shared/static/n8l0l1b3tg32wrz
 alls: stringdb ensembldb degree_barplot
 
 stringdb:
-	curl $(string_link) | gunzip >> stringdb.txt
+	curl $(string_link) | gunzip > stringdb.txt
 
 ensembldb:
-	curl -L $(ensembl_link) >> ensembldb.txt 
+	curl -L $(ensembl_link) > ensembldb.txt 
 
 degree_barplot: degree_barplot.py
 	python3 degree_barplot.py
--- degree_barplot.py
+++ degree_barplot.py
@@ -9,7 +9,7 @@ import pathlib
 import os
 
 df = pd.read_csv('stringdb.txt', sep=' ',low_memory=False)
-mydf = df[(df['combined_score'] >= '500')]
+mydf = df[(df['combined_score'] >= 500)]
 mydf[['ignore1', 'protein1']] = mydf['protein1'].str.split('.',expand=True)
 mydf[['ignore2', 'protein2']] = mydf['protein2'].str.split('.',expand=True)
 
@@ -37,7 +37,7 @@ final_db = pd.concat([hd_ensembl,ld_ensembl], axis=0)
 
 plot = final_db.groupby(['Degree', 'Protein stable ID'])['Pfam ID'].count().to_frame(name="Pfam ID").reset_index()
 
-sns.barplot(x="Degree", y="Pfam ID", palette="vlag", data=plot)
+sns.boxplot(x="Degree", y="Pfam ID", data=plot)
 plt.savefig('protein_domains_vs_string_degree.png', dpi=300, bbox_inches='tight')
 
 cwd=os.getcwd()
